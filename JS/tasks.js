593d90f1d132c94c8865013c8393710e0c970cb7
FiNiShYy <kauagabrielramkg@gmail.com>
2024-08-06 20:14:36 -0300

    update task.js

---
+++ JS/tasks.js
@@ -0,0 +1,14 @@
+const variavelString = "Kauã";
+const variavelNumber = 20;
+const variavelBoolean = true;
+
+console.log(
+            "TIPO VARIAVEL KAUÃ:", typeof variavelString,
+            "TIPO VARIAVEL 20:", typeof variavelNumber,
+            "TIPO VARIAVEL TRUE:", typeof variavelBoolean
+)
+
+//======================================================================//
+
+const primeiroNome = "Kauã";
+const ultimoNome = "Miguel";
