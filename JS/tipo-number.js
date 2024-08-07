4826f91ad42545ba9907483ad88970bb0085cd59
FiNiShYy <kauagabrielramkg@gmail.com>
2024-08-07 17:43:35 -0300

    add tipo-number.js

---
+++ JS/tipo-number.js
@@ -0,0 +1,14 @@
+const notaPrimeiroBi = 8;
+const notaSegundoBi = 6.3;
+const notaTerceiroBi = -2;
+const notaQuartoBi = Number.parseInt('5');
+
+const total = (notaPrimeiroBi + notaSegundoBi + notaTerceiroBi + notaQuartoBi) / 4;
+
+console.log("A média é:", total.toFixed(2));
+
+const nome = 'Kauã';
+console.log('Meu nome é ' + nome);
+
+//Number.parseInt para transformar string em Number
+//toFixed(2) Para definir apenas 2 casas após o ponto
