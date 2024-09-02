9f0ddcb6fda3c04edeb41b276122948bede436a5
FiNiShYy <kauagabrielramkg@gmail.com>
2024-09-02 18:34:52 -0300

    update index.js

--- CONHECENDO ARRAYS JS/index.js
+++ CONHECENDO ARRAYS JS/index.js
@@ -11,3 +11,9 @@ console.log(arrayNumsMultiplicados);
 //    const arraySomada = arrayNums.map(multiplicaPorDez)
 
 // console.log(arraySomada)
+
+const numeros = [43, 50, 65, 12]
+
+const soma = numeros.reduce((acum, atual) => atual + acum, 0)
+
+console.log(soma) //170
