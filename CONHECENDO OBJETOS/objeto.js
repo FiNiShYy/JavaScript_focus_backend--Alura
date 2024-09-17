d2d47f981b42e506b4d372f1c2874c528dfd489e
FiNiShYy <kauagabrielramkg@gmail.com>
2024-09-17 19:32:39 -0300

    conhecendo objetos folder and objeto.js added

---
+++ CONHECENDO OBJETOS/objeto.js
@@ -0,0 +1,11 @@
+const listaCPFs = ['12312312312', '12312312344'];
+
+const infosPessoa = ['nome', 'José Silva', 'idade', 32];
+console.log(infosPessoa[1]);
+
+const objPessoa = {
+    nome: "Kauã Gabriel",
+    idade: 20,
+    cpf: 12312312312,
+    turma: "JavaScript"
+}
