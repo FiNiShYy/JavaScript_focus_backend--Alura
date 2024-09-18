85d830293a75b6e9f05a2c9392d6f83c22c19b50
FiNiShYy <kauagabrielramkg@gmail.com>
2024-09-18 18:50:31 -0300

    alterando-objeto.js added

---
+++ CONHECENDO OBJETOS/alterando-objeto.js
@@ -0,0 +1,9 @@
+const estudante = {
+    nome: "Kauã Gabriel",
+    idade: 20,
+    cpf: "12312312312",
+    turma: "JavaScript"
+}
+estudante.telefone = "44999999999"
+
+console.log(estudante.telefone);
