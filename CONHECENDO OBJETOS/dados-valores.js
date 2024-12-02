76adcfbac240e5e909bd32cc05d6c743888d7336
FiNiShYy <kauagabrielramkg@gmail.com>
2024-12-02 19:38:06 -0300

    add dados-valores.js

---
+++ CONHECENDO OBJETOS/dados-valores.js
@@ -0,0 +1,10 @@
+const estudante = {
+    nome: "Kauã Gabriel",
+    idade: 21,
+    cpf: "12312312312",
+    turma: "JavaScript",
+    bolsista: true,
+    telefones: ["5544999999996" , "5544999999997"]
+}
+
+console.log("TCL: telefones", estudante.telefones[0])
