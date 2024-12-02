2d6f71dc3d046e29d58db3b9e320b3dcb86d4d95
FiNiShYy <kauagabrielramkg@gmail.com>
2024-12-02 19:38:54 -0300

    add objs-aninhados.js

---
+++ CONHECENDO OBJETOS/objs-aninhados.js
@@ -0,0 +1,16 @@
+const estudante = {
+    nome: "Kauã Gabriel",
+    idade: 21,
+    cpf: "12312312312",
+    turma: "JavaScript",
+    bolsista: true,
+    telefones: ["5544999999996" , "5544999999997"]
+}
+
+estudante.endereco = {
+    rua: "Av Brasil",
+    numero: "45A",
+    complemento: "APTO 101",
+}
+
+console.log(estudante.endereco);
