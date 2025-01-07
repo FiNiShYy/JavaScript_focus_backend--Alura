a3b62a108e2e17cc8ecc4a298357a19f66912780
FiNiShYy <kauagabrielramkg@gmail.com>
2025-01-07 20:11:31 -0300

    adicionar função para quebrar texto em parágrafos e contar palavras duplicadas

--- NODEJS/src/index.js
+++ NODEJS/src/index.js
@@ -6,7 +6,17 @@ const link = caminhoArquivo[2]
 
 fs.readFile(link, 'utf8', (err, texto) => {
 console.log(verificaPalavrasDuplicadas(texto))
+console.log(quebraEmParagrafos(texto))
 })
+
+
+function quebraEmParagrafos (texto) {
+    const paragrafos = texto.toLowerCase().split('\n')
+    const contagem = paragrafos.map((paragrago) => {
+        return verificaPalavrasDuplicadas(paragrago)
+    })
+    console.log(contagem);
+}
 
 function verificaPalavrasDuplicadas (texto) {
     const listaPalavras = texto.split(' ')
@@ -14,6 +24,6 @@ function verificaPalavrasDuplicadas (texto) {
     listaPalavras.forEach(palavra => {
         resultado[palavra] = (resultado[palavra] || 0) + 1 
     });
-    console.log(resultado);
+    return resultado
     
 }
