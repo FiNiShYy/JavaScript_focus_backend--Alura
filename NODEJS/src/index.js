4a2434acbbc138ad0e654ad2cd99d9dd10626d79
FiNiShYy <kauagabrielramkg@gmail.com>
2025-01-10 19:46:14 -0300

    ajustar tratamento de erros ao ler arquivo e melhorar legibilidade do código

--- NODEJS/src/index.js
+++ NODEJS/src/index.js
@@ -1,45 +1,43 @@
 const fs = require('fs');
-import trataErros from './funcoesErro';
+const trataErros = require('./erros/funcoesErro');
 
 const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2]
+const link = caminhoArquivo[2]; 
 
-
-fs.readFile(link, 'utf8', (err, texto) => {
+fs.readFile(link, 'utf-8', (erro, texto) => {
     try {
-    if (err) throw err;
-        contaPalavras(texto)
-    } catch(err) {
-        trataErros(err)
+        contaPalavras(texto);
+        if (erro) throw erro
+    } catch(erro) {
+        trataErros(erro);
     }
 })
 
 function contaPalavras(texto) {
-    const paragrafos = extraiParagrafos(texto)
-    const contagem = paragrafos.flatMap((paragrafo) => {
-        if (!paragrafo) return []
-        return verificaPalavrasDuplicadas(paragrafo)
-    })
-    console.log(contagem);
+  const paragrafos = extraiParagrafos(texto)
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return [];
+    return verificaPalavrasDuplicadas(paragrafo);
+  })
+  console.log(contagem);
 }
 
 function extraiParagrafos(texto) {
-    return texto.toLowerCase().split('\n')
+  return texto.toLowerCase().split('\n');
 }
 
-function limpaPalavras (palavra) {
-    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
 }
 
-function verificaPalavrasDuplicadas (texto) {
-    const listaPalavras = texto.split(' ')
-    const resultado = {}
-    listaPalavras.forEach(palavra => {
-        if (palavra.length >= 3 ) {
-            const palavraLimpa = limpaPalavras(palavra)
-            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1 
-        }
-    });
-    return resultado
-    
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(' ');
+  const resultado = {};
+  listaPalavras.forEach(palavra => {
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
+    }
+  })
+  return resultado;
 }
