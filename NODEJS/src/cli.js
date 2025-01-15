562b2a69ce90bb5406e9005e355326e2cd6aafcb
FiNiShYy <kauagabrielramkg@gmail.com>
2025-01-15 18:59:27 -0300

    adicionar processamento de arquivos com caminhos absolutos e refatorar função de leitura

--- NODEJS/src/cli.js
+++ NODEJS/src/cli.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import path from 'path';
 import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
 import { montaSaidaArquivo } from './helpers.js';
@@ -7,32 +8,44 @@ import { Command } from "commander"
 const program = new Command()
 
 program
-    .version("0.0.1")
-    .option("-t, --texto <string>", "Caminho do texto a ser processado")
-    .option("-d, --destino <string>", "Caminho da pasta onde salvar arquivo de resumos e de resultados")
-    .action((options) => {
-        const {texto, destino} = options
+.version("0.0.1")
+.option("-t, --texto <string>", "Caminho do texto a ser processado")
+.option("-d, --destino <string>", "Caminho da pasta onde salvar arquivo de resumos e de resultados")
+.action((options) => {
+    const {texto, destino} = options
+    
+    if (!texto || !destino ) {
+        console.error("Erro, favor inserir caminho de origem e destino!")
+        program.help()
+        return
+    }
+    
+    const caminhoTexto = path.resolve(texto)
+    const caminhoDestino = path.resolve(destino)
 
-        if (!texto || !destino ) {
-            console.error("Erro, favor inserir caminho de origem e destino!")
-            program.help()
-            return
-        }
-    })
-
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2]; 
-const endereco = caminhoArquivo[3]; 
-
-fs.readFile(link, 'utf-8', (erro, texto) => {
     try {
-        if (erro) throw erro
-        const resultado = contaPalavras(texto);
-        criaESalvaArquivo(resultado, endereco)
+        processsaArquivo(caminhoTexto, caminhoDestino)
     } catch(erro) {
-        trataErros(erro);
+        console.log("Ocorreu um erro no processamento!", erro);
     }
 })
+
+program.parse()
+
+function processsaArquivo(texto, destino) {
+    
+    fs.readFile(texto, 'utf-8', (erro, texto) => {
+        try {
+            if (erro) throw erro
+            const resultado = contaPalavras(texto);
+            criaESalvaArquivo(resultado, destino)
+            console.log("Texto processado com sucesso!");
+        } catch(erro) {
+            trataErros(erro);
+        }
+    })
+    
+}
 
 async function criaESalvaArquivo(listaPalavras, endereco) {
     const arquivoNovo = `${endereco}/resultado.txt`
