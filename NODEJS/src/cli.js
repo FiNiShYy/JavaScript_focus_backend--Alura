9d34f067faf77f2ec0a429648bbea2fa69876a21
FiNiShYy <kauagabrielramkg@gmail.com>
2025-01-15 19:02:47 -0300

    adicionar dependência 'chalk' e implementar estilização de mensagens no CLI

--- NODEJS/src/cli.js
+++ NODEJS/src/cli.js
@@ -4,6 +4,7 @@ import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
 import { montaSaidaArquivo } from './helpers.js';
 import { Command } from "commander"
+import chalk from "chalk"
 
 const program = new Command()
 
@@ -15,7 +16,7 @@ program
     const {texto, destino} = options
     
     if (!texto || !destino ) {
-        console.error("Erro, favor inserir caminho de origem e destino!")
+        console.error(chalk.red("Erro, favor inserir caminho de origem e destino!"))
         program.help()
         return
     }
@@ -26,7 +27,7 @@ program
     try {
         processsaArquivo(caminhoTexto, caminhoDestino)
     } catch(erro) {
-        console.log("Ocorreu um erro no processamento!", erro);
+        console.log(chalk.red("Ocorreu um erro no processamento!", erro));
     }
 })
 
@@ -39,7 +40,7 @@ function processsaArquivo(texto, destino) {
             if (erro) throw erro
             const resultado = contaPalavras(texto);
             criaESalvaArquivo(resultado, destino)
-            console.log("Texto processado com sucesso!");
+            console.log(chalk.green("Texto processado com sucesso!"));
         } catch(erro) {
             trataErros(erro);
         }
