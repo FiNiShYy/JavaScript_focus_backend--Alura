2c88aee96eeb501a783a3b3fcea373b2d1cf31e0
FiNiShYy <kaua@saltsystems.com.br>
2025-07-28 17:11:21 -0300

    adicionar arquivo index.js com importações de User, Admin e Docente e instância de novoUser

---
+++ JS OO/index.js
@@ -0,0 +1,9 @@
+import User from "./User.js";
+import Admin from "./Admin.js";
+import Docent from "./Docente.js";
+
+const novoUser = new User("Kauã", "k@k.com", "2024-01-01");
+// console.log(novoUser.exibirInfos());
+
+console.log(novoUser.exibirInfos());
+
