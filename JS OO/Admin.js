6d15ea717ce1818614e7ef0766ce511de4800885
FiNiShYy <kaua@saltsystems.com.br>
2025-07-28 17:11:12 -0300

    exportar a classe Admin como padrão e remover instância de teste

--- JS OO/Admin.js
+++ JS OO/Admin.js
@@ -1,6 +1,6 @@
 import User from './User.js';
 
-class Admin extends User {
+export default class Admin extends User {
     constructor(nome, email, nascimento, role = "admin", ativo = true) {
     super(nome, email, nascimento, role, ativo)
     }
@@ -9,8 +9,3 @@ class Admin extends User {
         return `Curso de ${nomeCurso} criado com ${qtdVagas} vagas`
     }
 }
-
-const novoAdmin = new Admin("Mariana", "m@m.com", "2024-01-01");
-
-console.log("🚀 - novoAdmin:", novoAdmin)
-console.log("🚀 - novoAdmin:", novoAdmin.criarCurso("Engenharia de Software", 30))
